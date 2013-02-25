7f03be4e34a00e501f598668d736ce4818a5bafc
Dylan Ethen <dlethe10@smumn.edu>
2013-02-24 21:06:24 -0600

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,12 +31,12 @@ describe "Authentication" do
 			before { sign_in user }
 
 			it { should have_selector('title', text: user.name) }
-
+			
 			it { should have_link('Users', 	  	 href: users_path) }
 			it { should have_link('Profile',     href: user_path(user)) }
 			it { should have_link('Settings', 	 href: edit_user_path(user)) }
 			it { should have_link('Sign out', 	 href: signout_path) }
-
+			
 			it { should_not have_link('Sign in', href: signin_path) }
 
 			describe "followed by signout" do
@@ -82,6 +82,21 @@ describe "Authentication" do
 				describe "submitting to the update action" do
 					before { put user_path(user) }
 					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
+			describe "in the Microposts controller" do
+
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before do
+						micropost = FactoryGirl.create(:micropost)
+						delete micropost_path(micropost)
+					end
 				end
 			end
 		end
